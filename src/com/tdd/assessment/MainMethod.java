8e6347e9221ebf67a39d570ff2339d6b1377d4f1
spandey232 <ms.shubhampandey05@gmail.com>
2021-01-06 23:57:13 +0530

    Addition with negatives not allowed

--- src/com/tdd/assessment/MainMethod.java
+++ src/com/tdd/assessment/MainMethod.java
@@ -1,5 +1,7 @@
 package com.tdd.assessment;
 
+import java.util.ArrayList;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,31 +10,73 @@ public class MainMethod {
 	@Test
 	public static void main(String[] args) {
 		StringCalculator obj=new StringCalculator();
-		Assert.assertEquals(0, obj.AddSupportDifferentDelimeter(""));
-		Assert.assertEquals(0, obj.AddSupportDifferentDelimeter(","));
-		Assert.assertEquals(1, obj.AddSupportDifferentDelimeter("1"));
-		Assert.assertEquals(1+2, obj.AddSupportDifferentDelimeter("1,2"));
-		Assert.assertEquals(1+2, obj.AddSupportDifferentDelimeter("1,2"));
-		Assert.assertEquals(1+2+3, obj.AddSupportDifferentDelimeter("1,2,3"));
-		//Assert.assertEquals(NumberFormatException.class, obj.Add_with_handle_new_line("1,2,33333333333"));     Number Format Exception 
+		Assert.assertEquals(0, obj.AddThrowNegNumException(""));
+		Assert.assertEquals(0, obj.AddThrowNegNumException(","));
+		Assert.assertEquals(1, obj.AddThrowNegNumException("1"));
+		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1,2"));
+		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1,2"));
+		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1,2,3"));
+		//Assert.assertEquals(NumberFormatException.class, obj.Add_with_handle_new_line("1,2,33333333333"));   //  throws  "Not a Valid Number"
 		
-		Assert.assertEquals(0, obj.AddSupportDifferentDelimeter("\n"));
-		Assert.assertEquals(1, obj.AddSupportDifferentDelimeter("\n1"));
-		Assert.assertEquals(1+2+3, obj.AddSupportDifferentDelimeter("1,2\n3"));
-		Assert.assertEquals(1+2, obj.AddSupportDifferentDelimeter("1\n2"));
-		Assert.assertEquals(1+2+3, obj.AddSupportDifferentDelimeter("1\n2\n3"));
+		Assert.assertEquals(0, obj.AddThrowNegNumException("\n"));
+		Assert.assertEquals(1, obj.AddThrowNegNumException("\n1"));
+		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1,2\n3"));
+		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1\n2"));
+		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1\n2\n3"));
 		
-		Assert.assertEquals(0, obj.AddSupportDifferentDelimeter("//:\n"));
-		Assert.assertEquals(6, obj.AddSupportDifferentDelimeter("//'\n1'2'3"));
-		Assert.assertEquals(1, obj.AddSupportDifferentDelimeter("//:\n1"));
-		Assert.assertEquals(1+2+3, obj.AddSupportDifferentDelimeter("//;\n1;2;3"));
-		Assert.assertEquals(1+2, obj.AddSupportDifferentDelimeter("//:\n1:2"));
-		Assert.assertEquals(1+2+3, obj.AddSupportDifferentDelimeter("//,\n1,2,3"));
+		Assert.assertEquals(0, obj.AddThrowNegNumException("//:\n"));
+		Assert.assertEquals(6, obj.AddThrowNegNumException("//'\n1'2'3"));
+		Assert.assertEquals(1, obj.AddThrowNegNumException("//:\n1"));
+		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("//;\n1;2;3"));
+		Assert.assertEquals(1+2, obj.AddThrowNegNumException("//:\n1:2"));
+		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("//,\n1,2,3"));
 
+		Assert.assertEquals(0, obj.AddThrowNegNumException("//:\n"));
+		//Assert.assertEquals(RuntimeException.class, obj.AddThrowNegNumException("//'\n1'2'-3"));			throws  "negatives not allowed[-3]"
+		//Assert.assertEquals(RuntimeException.class, obj.AddThrowNegNumException("//'\n-1'-2'-3"));        throws  "negatives not allowed[-1, -2, -3]"
+		
 	}
 }
 class StringCalculator{
 	
+	public int AddThrowNegNumException(String numbers){
+		if(numbers=="") return 0;
+		else{
+			int sum=0;
+			ArrayList<Integer> negativeValues=new ArrayList<>();
+			if(numbers.startsWith("//")){
+				String numberPart=numbers.substring(numbers.indexOf("\n")+1);
+				String[] splitArr=numberPart.split(numbers.substring(2,3));
+				for(int i=0;i<splitArr.length;i++){
+					if(splitArr[i].isEmpty()==false){
+						try{
+							if(Integer.parseInt(splitArr[i])<0)
+						    	negativeValues.add(Integer.parseInt(splitArr[i]));
+							sum+=Integer.parseInt(splitArr[i]);
+						}catch(Exception e){
+								throw new NumberFormatException("Not a Valid Number");
+						}
+					}
+				}
+			}else{
+				String[] splitArr=numbers.split(",|\n");
+				for(int i=0;i<splitArr.length;i++){
+					if(splitArr[i].isEmpty()==false){
+						try{
+							if(Integer.parseInt(splitArr[i])<0)
+						    	negativeValues.add(Integer.parseInt(splitArr[i]));
+							sum+=Integer.parseInt(splitArr[i]);
+						}catch(Exception e){
+								throw new NumberFormatException("Not a Valid Number");
+						}
+					}
+				}
+			}
+			if(negativeValues.size()>0)
+				throw new RuntimeException("negatives not allowed"+negativeValues);
+			return sum;
+		}
+	}
 	public int AddSupportDifferentDelimeter(String numbers){
 		if(numbers=="") return 0;
 		else{
