cef94aff86dac8f104e75b532ea4a0a5414102a2
spandey232 <ms.shubhampandey05@gmail.com>
2021-01-07 00:20:36 +0530

    Ignore more than 1000 values

--- src/com/tdd/assessment/MainMethod.java
+++ src/com/tdd/assessment/MainMethod.java
@@ -10,35 +10,87 @@ public class MainMethod {
 	@Test
 	public static void main(String[] args) {
 		StringCalculator obj=new StringCalculator();
-		Assert.assertEquals(0, obj.AddThrowNegNumException(""));
-		Assert.assertEquals(0, obj.AddThrowNegNumException(","));
-		Assert.assertEquals(1, obj.AddThrowNegNumException("1"));
-		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1,2"));
-		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1,2"));
-		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1,2,3"));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000(""));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000(","));
+		Assert.assertEquals(1, obj.AddIgnoreMoreThan1000("1"));
+		Assert.assertEquals(1+2, obj.AddIgnoreMoreThan1000("1,2"));
+		Assert.assertEquals(1+2, obj.AddIgnoreMoreThan1000("1,2"));
+		Assert.assertEquals(1+2+3, obj.AddIgnoreMoreThan1000("1,2,3"));
 		//Assert.assertEquals(NumberFormatException.class, obj.Add_with_handle_new_line("1,2,33333333333"));   //  throws  "Not a Valid Number"
 		
-		Assert.assertEquals(0, obj.AddThrowNegNumException("\n"));
-		Assert.assertEquals(1, obj.AddThrowNegNumException("\n1"));
-		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1,2\n3"));
-		Assert.assertEquals(1+2, obj.AddThrowNegNumException("1\n2"));
-		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("1\n2\n3"));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000("\n"));
+		Assert.assertEquals(1, obj.AddIgnoreMoreThan1000("\n1"));
+		Assert.assertEquals(1+2+3, obj.AddIgnoreMoreThan1000("1,2\n3"));
+		Assert.assertEquals(1+2, obj.AddIgnoreMoreThan1000("1\n2"));
+		Assert.assertEquals(1+2+3, obj.AddIgnoreMoreThan1000("1\n2\n3"));
 		
-		Assert.assertEquals(0, obj.AddThrowNegNumException("//:\n"));
-		Assert.assertEquals(6, obj.AddThrowNegNumException("//'\n1'2'3"));
-		Assert.assertEquals(1, obj.AddThrowNegNumException("//:\n1"));
-		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("//;\n1;2;3"));
-		Assert.assertEquals(1+2, obj.AddThrowNegNumException("//:\n1:2"));
-		Assert.assertEquals(1+2+3, obj.AddThrowNegNumException("//,\n1,2,3"));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000("//:\n"));
+		Assert.assertEquals(6, obj.AddIgnoreMoreThan1000("//'\n1'2'3"));
+		Assert.assertEquals(1, obj.AddIgnoreMoreThan1000("//:\n1"));
+		Assert.assertEquals(1+2+3, obj.AddIgnoreMoreThan1000("//;\n1;2;3"));
+		Assert.assertEquals(1+2, obj.AddIgnoreMoreThan1000("//:\n1:2"));
+		Assert.assertEquals(1+2+3, obj.AddIgnoreMoreThan1000("//,\n1,2,3"));
 
-		Assert.assertEquals(0, obj.AddThrowNegNumException("//:\n"));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000("//:\n"));
 		//Assert.assertEquals(RuntimeException.class, obj.AddThrowNegNumException("//'\n1'2'-3"));			throws  "negatives not allowed[-3]"
 		//Assert.assertEquals(RuntimeException.class, obj.AddThrowNegNumException("//'\n-1'-2'-3"));        throws  "negatives not allowed[-1, -2, -3]"
+		
+		Assert.assertEquals(3, obj.AddIgnoreMoreThan1000("//'\n1'2'3000"));
+		Assert.assertEquals(0, obj.AddIgnoreMoreThan1000("//:\n1100"));
+		Assert.assertEquals(1+3, obj.AddIgnoreMoreThan1000("//;\n1;2000;3"));
+		Assert.assertEquals(1002, obj.AddIgnoreMoreThan1000("//:\n1000:2"));
+		Assert.assertEquals(1+3, obj.AddIgnoreMoreThan1000("//,\n1,2000,3"));
 		System.out.println(obj.GetCalledCount());
 	}
 }
 class StringCalculator{
 	int methodCallCount=0;
+	public int AddIgnoreMoreThan1000(String numbers){
+		synchronized(this) {
+			methodCallCount++;
+	    }
+		if(numbers=="") return 0;
+		else{
+			int sum=0;
+			ArrayList<Integer> negativeValues=new ArrayList<>();
+			if(numbers.startsWith("//")){
+				String numberPart=numbers.substring(numbers.indexOf("\n")+1);
+				String[] splitArr=numberPart.split(numbers.substring(2,3));
+				for(int i=0;i<splitArr.length;i++){
+					if(splitArr[i].isEmpty()==false){
+						try{
+							int value=Integer.parseInt(splitArr[i]);
+							if(value<0)
+						    	negativeValues.add(value);
+							if(value<=1000)
+							    sum+=value;
+						}catch(Exception e){
+								throw new NumberFormatException("Not a Valid Number");
+						}
+					}
+				}
+			}else{
+				String[] splitArr=numbers.split(",|\n");
+				for(int i=0;i<splitArr.length;i++){
+					if(splitArr[i].isEmpty()==false){
+						try{
+							int value=Integer.parseInt(splitArr[i]);
+							if(value<0)
+						    	negativeValues.add(value);
+							if(value<=1000)
+								sum+=value;
+						}catch(Exception e){
+								throw new NumberFormatException("Not a Valid Number");
+						}
+					}
+				}
+			}
+			if(negativeValues.size()>0)
+				throw new RuntimeException("negatives not allowed"+negativeValues);
+			return sum;
+		}
+	}
+	
 	public int AddThrowNegNumException(String numbers){
 		synchronized(this) {
 			methodCallCount++;
@@ -80,6 +132,7 @@ class StringCalculator{
 			return sum;
 		}
 	}
+	
 	public int AddSupportDifferentDelimeter(String numbers){
 		if(numbers=="") return 0;
 		else{
@@ -111,6 +164,7 @@ class StringCalculator{
 			return sum;
 		}
 	}
+	
 	public int Add_with_handle_new_line(String numbers){
 		if(numbers=="") return 0;
 		else{
@@ -128,6 +182,7 @@ class StringCalculator{
 			return sum;
 		}
 	}
+	
 	public int Add_with_handle_unknown_amount(String numbers){
 		if(numbers=="") return 0;
 		else{
@@ -145,6 +200,7 @@ class StringCalculator{
 			return sum;
 		}
 	}
+	
 	public int Add(String numbers){
 		if(numbers=="") return 0;
 		else{
@@ -162,6 +218,7 @@ class StringCalculator{
 			return sum;
 		}
 	}
+	
 	public int GetCalledCount(){
 		return methodCallCount;
 	}
